16215efbb3950e3d9a1b178329924b8aa72e7691
Mehdi <pichardmehdi@gmail.com>
2021-05-16 12:59:31 +0200

    la

--- commands/count.js
+++ commands/count.js
@@ -6,7 +6,7 @@ module.exports=
     {
         const guild = client.guilds.cache.get('836500571590098966');
         const  memberCount= guild.memberCount;
-        message.channel.send(memberCount);
+        message.channel.send('nous sommes : '+memberCount+"pirates");
         console.log(memberCount);
 }
 }
