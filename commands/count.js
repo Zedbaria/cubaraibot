8281a37a7f58bba0ba8d359e9e8f3b129bbeb2d7
Mehdi <pichardmehdi@gmail.com>
2021-05-16 12:57:38 +0200

    la

--- commands/count.js
+++ commands/count.js
@@ -1,8 +1,12 @@
-module.exports = async(client)=>
+module.exports=
 {
+    name:'count',
+    description : "compteur",
+    execute(client,message,args,Discord)
+    {
         const guild = client.guilds.cache.get('836500571590098966');
         const  memberCount= guild.memberCount;
         message.channel.send(memberCount);
         console.log(memberCount);
- 
+}
 }
