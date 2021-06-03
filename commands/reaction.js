6a8cd9676281cb091175ad1d8f9ee85477a2a6b6
Mehdi <pichardmehdi@gmail.com>
2021-06-03 16:42:20 +0200

    test reaction

--- commands/reaction.js
+++ commands/reaction.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'reaction',
     description: "pour se donner des rôles",
     async execute(client, message, args, Discord ){
-        const channel = '843218877714530315';
+        const channel = '836500749314818078';
         const verif = message.guild.roles.cache.find(role => role.name ==="Membres");
 
         const teamdbzemoji = '✅';
@@ -14,6 +14,7 @@ module.exports = {
  
         let messageEmbed = await message.channel.send(embed);
         messageEmbed.react(teamdbzemoji);
+        
         client.on('messageReactionAdd', async (reaction, user) => {
             if (reaction.message.partial) await reaction.message.fetch();
             if (reaction.partial) await reaction.fetch();
