88ac91fb5ed7d62751fb8aa0b823a025252a7ecd
Mehdi <pichardmehdi@gmail.com>
2021-06-05 16:13:39 +0200

    ajout status

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,13 +1,5 @@
 module.exports=()=>
 {
-    
-        console.log(`Logged in as ${client.user.tag}!`);
-        client.user.setPresence({
-            status: "online",  //You can show online, idle....
-            game: {
-                name: "PLAY.CUBARAIBE.FR",  //The message shown
-                type: "STREAMING" //PLAYING: WATCHING: LISTENING: STREAMING:
-            }
-        });
+    console.log('Bot en ligne');
     
 }
